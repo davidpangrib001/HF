31e6abed7e48da65c1af310869998881103f84db
It's Davitt <125800740+MyDapitt@users.noreply.github.com>
2023-06-09 19:16:26 +0700

    Update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const CFonts  = require('cfonts');
+const CFonts = require('cfonts');
 const chalk = require('chalk');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
@@ -24,12 +24,12 @@ const PORT = process.env.PORT || port;
 connectMongoDb();
 
 app.set('trust proxy', 1);
-app.use(compression())
+app.use(compression());
 
 const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, 
-  max: 2000, 
-  message: 'Oops too many requests'
+  windowMs: 1 * 60 * 1000,
+  max: 2000,
+  message: 'Oops too many requests',
 });
 app.use(limiter);
 
@@ -38,7 +38,7 @@ app.use(expressLayout);
 app.use(express.static('public'));
 
 app.use(session({
-  secret: 'secret',  
+  secret: 'secret',
   resave: true,
   saveUninitialized: true,
   cookie: { maxAge: 86400000 },
@@ -56,67 +56,53 @@ require('./lib/config')(passport);
 
 app.use(flash());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
   res.locals.user = req.user || null;
   next();
-})
-
-/*app.get('/', (req, res) => {
-  res.render('index', {
-    layout: 'layouts/main'
-  });
 });
-*/
 
-app.get('/docs', isAuthenticated, async (req, res) => { 
-  let getkey = await getApikey(req.user.id)
-  let { apikey, username } = getkey
+app.get('/docs', isAuthenticated, async (req, res) => {
+  let getkey = await getApikey(req.user.id);
+  let { apikey, username } = getkey;
   res.render('docs', {
     username: username,
-    apikey: apikey
-//     layout: 'layouts/main'
+    apikey: apikey,
   });
 });
 
 app.get('/price', (req, res) => {
-  res.render('buyFull', {
-//     layout: 'layouts/main'
-  })
-})
+  res.render('buyFull');
+});
 
 app.get('/premium', (req, res) => {
-  res.render('buyFull', {
-//     layout: 'layouts/main'
-  })
-})
+  res.render('buyFull');
+});
 
 app.use('/api', apiRouters);
 app.use('/users', userRouters);
 
-/*app.use(function (req, res, next) {
+app.use(function (req, res, next) {
   if (res.statusCode == '200') {
-    res.render('notfound', {
-      layout: 'layouts/main'
-    });
+    res.render('notfound');
   }
-*/
 });
+
 CFonts.say('Onee API', {
   font: 'block',
   align: 'center',
   gradient: ['red', 'magenta']
-})
+});
 
 CFonts.say(`Recode By David`, {
   font: 'console',
   align: 'center',
   gradient: ['blue', 'yellow']
-})
+});
 
-console.log(chalk.yellow('Hai, Owner!'))
+console.log(chalk.yellow('Hai, Owner!'));
 app.set('json spaces', 4);
 
 app.listen(PORT, () => {
